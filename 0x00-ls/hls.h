3c33b1381e51adef348869c10cbbf070386c228d
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-04-15 22:02:30 +0100

    initial

--- 0x00-ls/hls.h
+++ 0x00-ls/hls.h
@@ -7,8 +7,15 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <stdint.h>
+#include <time.h>
+
 
 int _strcmp(char const *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+
 
 void read_local_file(void);
 void get_options(int argc, char const *argv[], int *options);
--- 0x00-ls/read_files.c
+++ 0x00-ls/read_files.c
@@ -15,7 +15,7 @@ void read_files(int argc, char const *argv[], int *op)
 		{
 			read_file(argv[i], op);
 		}
-		if ((i + 1) != argc && op[0] == 0)
+		if ((i + 1) != argc && op[0] == 0 && op[3] == 0)
 			printf("\n");
 		if ((i + 2) < argc && (op[0] == 1 || op[1] == 1 || op[2] == 1))
 			printf("\n");
@@ -61,7 +61,7 @@ void read_file(char const *name, int *op)
 			}
 			read = readdir(dir);
 		}
-		if (op[0] == 0 && op[1] == 0 && op[2] == 0)
+		if (op[0] == 0 && op[1] == 0 && op[2] == 0 && op[3] == 0)
 			printf("\n");
 	}
 	if (errno != 0)
@@ -75,7 +75,32 @@ void read_file(char const *name, int *op)
  */
 void print_file(struct dirent *read, int *op)
 {
-	printf("%s ", (*read).d_name);
+	struct stat fileStat;
+	struct group *grp;
+    struct passwd *user;
+	char time[24];
+
+	if (op[3] == 1)
+	{
+		_strncpy(time, ctime(&fileStat.st_mtime), 24);
+		lstat((*read).d_name, &fileStat);
+		printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+		printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
+		printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
+		printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
+		printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
+		printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
+		printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
+		printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
+		printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
+		printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
+		user = getpwuid(fileStat.st_uid);
+		grp = getgrgid(fileStat.st_gid);
+		printf(" %lu %s %s %li %s %s\n", fileStat.st_nlink, (*user).pw_name, (*grp).gr_name,
+		(intmax_t)fileStat.st_size, time, (*read).d_name);
+	}
+	else
+		printf("%s ", (*read).d_name);
 	if (op[0] == 1)
 		printf("\n");
 }
