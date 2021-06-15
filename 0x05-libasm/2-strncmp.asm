8675b94e9effb9f4455ac8ea7bd867b0463b1ff2
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-06-15 15:55:53 +0100

    fix substraction

--- 0x05-libasm/2-strncmp.asm
+++ 0x05-libasm/2-strncmp.asm
@@ -6,14 +6,13 @@ asm_strncmp:
 	push rbp					; push the base
 	mov rbp, rsp				; start new base
 
-	mov R8, 0h					; set return to 0
 	mov R9, 0h					; set counter to 0
 asm_loop:
 	mov al, [rdi + R9]			; store the char of first argument + counter in al
 	mov bl, [rsi + R9]			; store the char of second argument + counter in ab
 
 	cmp R9, rdx                 ; compare counter to n
-	je asm_end                  ; return if true
+	je asm_end_count                  ; return if true
 
 	cmp al, bl					; compare the char + counter of first and second argument
 	je equal_char				; if equal jump to equal_char
@@ -22,25 +21,31 @@ asm_loop:
 
 equal_char:
 	cmp al, 0h					; compare al to Null
-	mov R8, 0					; set return to 0 if true
 	je asm_end					; jmp to asm_end
 
 	inc R9						; increace counter
 	jmp asm_loop				; loop again
 
 al_big:
-	inc R9                      ; increace counter
-	neg R9                      ; negate counter
-	mov R8, R9					; set return to - counter
+	movzx R12, al
+	movzx R13, bl
+	sub R12, R13
 	jmp asm_end					; jmp to asm_end
 
 bl_big:
-	inc R9                      ; increace counter
-	mov R8, R9					; set return to counter
+	movzx R12, al
+	movzx R13, bl
+	sub R12, R13
 	jmp asm_end					; jmp to asm_end
 
 asm_end:
-	mov rax, R8					; pass the counter to the return register
+	mov rax, R12
+	mov rsp, rbp				; return to old base
+	pop rbp						; pop to the call base
+	ret
+
+asm_end_count:
+	mov rax, 0h
 	mov rsp, rbp				; return to old base
 	pop rbp						; pop to the call base
 	ret
