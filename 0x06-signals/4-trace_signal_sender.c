043b0f0479e20a5ce956c631afecfe21f02d6118
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-06-28 21:41:19 +0100

    task 4

--- 0x06-signals/4-trace_signal_sender.c
+++ 0x06-signals/4-trace_signal_sender.c
@@ -1,17 +1,26 @@
 #include "signals.h"
-
-void term_handle(int sig, siginfo_t *sig_info, void *p)
+/**
+ * print_sig - print signal
+ * @sig: signal
+ * @sig_info: siginfo_t
+ * @p: pointer
+ */
+void print_sig(int sig, siginfo_t *sig_info, void *p)
 {
 	(void)p;
 	(void)sig;
 	printf("SIGQUIT sent by %d\n", sig_info->si_pid);
 }
-
+/**
+ * trace_signal_sender - Entry point
+ *
+ * Return: 0 or -1
+ */
 int trace_signal_sender(void)
 {
 	struct sigaction new, old;
 
-	new.sa_sigaction = term_handle;
+	new.sa_sigaction = print_sig;
 	sigemptyset(&new.sa_mask);
 	new.sa_flags = 0;
 
