bf575cce18af63a0819bd7411e2d1aaa976dc60c
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-07-05 16:45:04 +0100

    initial

---
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -0,0 +1,22 @@
+#include "rb_trees.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+	rb_tree_t *tree;
+
+	tree = malloc(sizeof(rb_tree_t));
+	if (!tree)
+		return (NULL);
+
+	(*tree).parent = parent;
+	(*tree).color = color;
+	(*tree).n = value;
+	(*tree).left = NULL;
+	(*tree).right = NULL;
+
+	return (tree);
+}
