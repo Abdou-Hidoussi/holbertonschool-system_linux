4daf029e93a67214439d197719ed62c69ecccdb4
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-08-04 22:09:20 +0100

    dropin

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -20,31 +20,24 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, i;
-	char *s;
+	char *py_bytes_str;
+	Py_ssize_t py_bytes_size;
+	Py_ssize_t i;
 
-	puts("[.] bytes object info");
+	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
-		puts("  [ERROR] Invalid Bytes Object");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
+	py_bytes_size = (((PyVarObject *)(p))->ob_size);
+	py_bytes_str = (((PyBytesObject *)(p))->ob_sval);
 
-	size = PyBytes_Size(p);
-	if (size < 10)
-		size += 1;
-	else
-	{
-		size = 10;
-	}
+	printf("  size: %ld\n", py_bytes_size);
+	printf("  trying string: %s\n", py_bytes_str);
+	py_bytes_size = py_bytes_size > 9 ? 10 : py_bytes_size + 1;
+	printf("  first %ld bytes: ", py_bytes_size);
 
-	s = PyBytes_AsString(p);
-	printf("  size: %li\n"
-	"  trying string: %s\n"
-	"  first %li bytes:",
-	size, s, size);
-
-	for (i = 0; i <= size && i < 10; i++)
-		printf(" %02hhx", s[i]);
-	printf("\n");
+	for (i = 0; i < py_bytes_size; i++)
+		printf("%02hhx%c", py_bytes_str[i], i < py_bytes_size - 1 ? ' ' : '\n');
 }
