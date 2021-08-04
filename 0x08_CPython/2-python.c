75295266f16356e463027baec2fe67088b03ee5b
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-08-04 21:57:53 +0100

    change to puts for eror

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -15,7 +15,7 @@ void print_python_list(PyObject *p)
 		printf("Element %li: %s\n", c, Py_TYPE((PyList_GetItem(p, c)))->tp_name);
 }
 /**
- * print_python_bytes - print basic info about Python byte objects
+ * print_python_bytes - prints some basic info about Python lists
  * @p: Python object
  */
 void print_python_bytes(PyObject *p)
@@ -23,12 +23,13 @@ void print_python_bytes(PyObject *p)
 	Py_ssize_t size, i;
 	char *s;
 
-	printf("[.] bytes object info\n");
+	puts("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
-		printf("  [ERROR] Invalid Bytes Object\n");
+		puts("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
+
 	size = PyBytes_Size(p);
     if (size < 10) 
         size++;
@@ -36,6 +37,7 @@ void print_python_bytes(PyObject *p)
     {
         size = 10;
     }
+
 	s = PyBytes_AsString(p);
 	printf("  size: %li\n"
 	"  trying string: %s\n"
